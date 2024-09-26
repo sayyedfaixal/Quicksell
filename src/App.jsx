2cc94b957ddfde1797c7b732e6095a68c99a9fc6
Faisal Sayed <sayedfaisal544@gmail.com>
2024-09-26 22:12:58 +0530

    Add logo to the UI and fix minor issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
 import { useEffect, useState, useRef } from "react";
 import "./index.css";
 import Card from "./Components/Card";
+import noPriority from "./assets/noPriority.svg";
+import lowPriority from "./assets/lowPriority.svg";
+import mediumPriority from "./assets/mediumPriority.svg";
+import highPriority from "./assets/highPriority.svg";
+import urgentPriority from "./assets/urgentPriorityColour.svg";
+import add from "./assets/add.svg";
+import Dot3Menu from "./assets/Dot3Menu.svg";
+import toDo from "./assets/toDo.svg";
+import inProgress from "./assets/in-progress.svg";
+import backlog from "./assets/backlog.svg";
+import display from "./assets/display.svg";
 
 function App() {
   const [cards, setCards] = useState([]);
@@ -13,6 +24,20 @@ function App() {
   );
   const [showDropdown, setShowDropdown] = useState(false); // State to control dropdown visibility
   const dropdownRef = useRef(null); // Ref for the dropdown
+
+  // Close dropdown when clicking outside
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setShowDropdown(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [dropdownRef]);
 
   useEffect(() => {
     const fetchCards = async () => {
@@ -43,16 +68,17 @@ function App() {
           key = card.userId; // Group by userId
           break;
         case "priority":
-          key = card.priority;
+          key = card.priority; // Group by Priority
           break;
         default:
-          key = card.status;
+          key = card.status; // Group by Status
       }
 
       if (!groups[key]) {
-        groups[key] = [];
+        groups[key] = { cards: [], count: 0 }; // Initialize count
       }
-      groups[key].push(card);
+      groups[key].cards.push(card);
+      groups[key].count += 1; // Increment count
       return groups;
     }, {});
   };
@@ -67,6 +93,60 @@ function App() {
           : 0 // No sorting
     );
   };
+  const Circle = ({ letter }) => {
+    return (
+      <div className="w-8 h-8 flex items-center justify-center bg-gray-400 text-white rounded-full mr-2">
+        {letter}
+      </div>
+    );
+  };
+
+  // Priority mapping
+  const priorityLabels = {
+    4: (
+      <div className="flex gap-2">
+        <img src={urgentPriority} alt="Urgent" /> Urgent
+      </div>
+    ),
+    3: (
+      <div className="flex gap-2">
+        <img src={highPriority} alt="High" /> High
+      </div>
+    ),
+    2: (
+      <div className="flex gap-2">
+        <img src={mediumPriority} alt="Medium" /> Medium
+      </div>
+    ),
+    1: (
+      <div className="flex gap-2">
+        <img src={lowPriority} alt="Low Priority" /> Low
+      </div>
+    ),
+    0: (
+      <div className="flex gap-2">
+        <img src={noPriority} alt="No priority" /> No priority
+      </div>
+    ),
+  };
+
+  const statusLabels = {
+    "In progress": (
+      <div className="flex gap-2">
+        <img src={inProgress} alt="In Progress" /> In Progress
+      </div>
+    ),
+    Todo: (
+      <div className="flex gap-2">
+        <img src={toDo} alt="Todo" /> Todo
+      </div>
+    ),
+    Backlog: (
+      <div className="flex gap-2">
+        <img src={backlog} alt="Backlogs" /> Backlogs
+      </div>
+    ),
+  };
 
   const groupedCards = groupCards();
 
@@ -75,6 +155,50 @@ function App() {
       {" "}
       {/* Parent div with margin on left and right */}
       <nav className="bg-gray-800 p-4 flex justify-between items-center">
+        <div className="relative inline-block text-left" ref={dropdownRef}>
+          <button
+            type="button"
+            onClick={() => setShowDropdown(!showDropdown)} // Toggle dropdown
+            className="inline-flex items-center text-slate bg-white hover:bg-slate-700 hover:text-white focus:outline-none rounded-md px-4 py-2 ring-2 ring-blue"
+          >
+            <img src={display} className="mr-2" />
+            Display
+          </button>
+          {showDropdown && (
+            <div className="absolute left-5 mt-2 w-52 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+              <div
+                className="py-1"
+                role="menu"
+                aria-orientation="vertical"
+                aria-labelledby="options-menu"
+              >
+                <div className="px-4 py-2 text-sm text-gray-700 flex gap-2">
+                  <span className="font-semibold">Grouping:</span>
+                  <select
+                    value={groupBy}
+                    onChange={(e) => setGroupBy(e.target.value)}
+                    className="ml-2 p-1 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  >
+                    <option value="status">Status</option>
+                    <option value="user">User</option>
+                    <option value="priority">Priority</option>
+                  </select>
+                </div>
+                <div className="px-4 py-2 text-sm text-gray-700 flex gap-2">
+                  <span className="font-semibold">Sorting:</span>
+                  <select
+                    value={sortBy}
+                    onChange={(e) => setSortBy(e.target.value)}
+                    className="ml-2 p-1 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  >
+                    <option value="priority">Priority</option>
+                    <option value="title">Title</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
         <h1 className="text-white text-lg font-bold">QuickSell</h1>
         <div>
           <h2 className="text-white text-lg">
@@ -84,61 +208,54 @@ function App() {
             </span>
           </h2>
         </div>
-        <div className="relative inline-block text-left" ref={dropdownRef}>
-          <button
-            type="button"
-            onClick={() => setShowDropdown(!showDropdown)} // Toggle dropdown
-            className="inline-flex items-center text-white bg-gray-700 hover:bg-gray-600 focus:outline-none rounded-md px-4 py-2"
-          >
-            Display
-          </button>
-
-          {showDropdown && (
-            <div className="absolute right-0 z-10 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
-              <div
-                className="py-1"
-                role="menu"
-                aria-orientation="vertical"
-                aria-labelledby="options-menu"
-              >
-                <div className="px-4 py-2 text-sm text-gray-700">
-                  <span className="font-semibold">Grouping:</span>
-                  <select
-                    value={groupBy}
-                    onChange={(e) => setGroupBy(e.target.value)}
-                    className="ml-2 p-1 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                  >
-                    <option value="status">Group By Status</option>
-                    <option value="user">Group By User</option>
-                    <option value="priority">Group By Priority</option>
-                  </select>
-                </div>
-                <div className="px-4 py-2 text-sm text-gray-700">
-                  <span className="font-semibold">Sorting:</span>
-                  <select
-                    value={sortBy}
-                    onChange={(e) => setSortBy(e.target.value)}
-                    className="ml-2 p-1 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                  >
-                    <option value="priority">Sort By Priority</option>
-                    <option value="title">Sort By Title</option>
-                  </select>
-                </div>
-              </div>
-            </div>
-          )}
-        </div>
       </nav>
       <div className="flex mt-4">
-        {Object.entries(groupedCards).map(([key, group]) => (
+        {Object.entries(groupedCards).map(([key, { cards, count }]) => (
           <div key={key} className="flex-1">
-            <h2 className="font-semibold ml-2">
-              {groupBy === "user"
-                ? users.find((user) => user.id === key)?.name || key
-                : key}{" "}
-              {/* Conditional rendering */}
-            </h2>
-            {sortCards(group).map((card) => (
+            <div className="flex justify-between mr-2">
+              <h2 className="font-semibold ml-2 flex items-center">
+                {groupBy === "user" ? (
+                  <>
+                    <Circle
+                      letter={users
+                        .find((user) => user.id === key)
+                        ?.name.charAt(0)}
+                    />{" "}
+                    {/* Adding the profile icon as the first letter inside the circle */}
+                    {users.find((user) => user.id === key)?.name || key}
+                    <span className="ml-1">({count})</span>{" "}
+                    {/* Display count */}
+                  </>
+                ) : groupBy === "priority" ? (
+                  <>
+                    {priorityLabels[key]}
+                    <span className="ml-1">({count})</span>
+                    {/* Display count */}
+                  </>
+                ) : groupBy === "status" ? (
+                  <>
+                    {statusLabels[key]} <span className="ml-1">({count})</span>{" "}
+                    {/* Display count */}
+                  </>
+                ) : (
+                  <span className="ml-1">({count})</span> // Display count for other cases
+                )}
+              </h2>
+              <div className="flex justify-end gap-2">
+                <img
+                  src={add}
+                  style={{ width: "16px", height: "24px" }}
+                  alt="Add"
+                />
+                <img
+                  src={Dot3Menu}
+                  style={{ width: "16px", height: "24px" }}
+                  alt="Medium"
+                />
+              </div>
+            </div>
+
+            {sortCards(cards).map((card) => (
               <Card
                 key={card.id}
                 title={card.title}
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,3 +1,12 @@
+import noPriority from "../assets/noPriority.svg";
+import lowPriority from "../assets/lowPriority.svg";
+import mediumPriority from "../assets/mediumPriority.svg";
+import highPriority from "../assets/highPriority.svg";
+import urgentPriority from "../assets/urgentPriorityColour.svg";
+import toDo from "../assets/toDo.svg";
+import inProgress from "../assets/in-progress.svg";
+import backlog from "../assets/backlog.svg";
+
 const Card = ({
   title,
   id,
@@ -9,41 +18,35 @@ const Card = ({
   userName,
 }) => {
   return (
-    /**
-     * {
-            "id": "CAM-1",
-            "title": "Update User Profile Page UI",
-            "tag": [
-                "Feature request"
-            ],
-            "userId": "usr-1",
-            "status": "Todo",
-            "priority": 4
-        },
-         */
-
     <div className="border rounded-lg px-2 m-2 bg-gray-100">
       <div className="flex flex-col justify-between">
         <div className="flex justify-between mt-2">
           <div className="text-sm text-gray-500 font-semibold">{id}</div>
           <div className="w-8 h-8 bg-gray-300 text-white flex items-center justify-center rounded-full">
-            {userName ? userName.charAt(0) : userId.charAt(4)}{" "}
+            {userName ? userName.charAt(4) : userId.charAt(4)}{" "}
             {/* Display first character of userName or userId */}
           </div>
         </div>
-        <div className="text-sm font-semibold">{title}</div>
+        <div className="flex gap-2">
+          <div className="flex items-center">
+            {status === "Todo" && <img src={toDo} alt="Urgent" />}
+            {status === "Backlog" && <img src={backlog} alt="High" />}
+            {status === "In progress" && <img src={inProgress} alt="Medium" />}
+          </div>
+          <div className="text-sm font-semibold">{title}</div>
+        </div>
       </div>
       <div className="flex justify-start py-2 text-sm">
         <div>{points}</div>
-        <div className="mx-2 py-1">
-          {priority === 4 && <span>🚨 Urgent</span>}
-          {priority === 3 && <span>🔥 High</span>}
-          {priority === 2 && <span>⚠️ Medium</span>}
-          {priority === 1 && <span>🔽 Low</span>}
-          {priority === 0 && <span>🔓 No priority</span>}
+        <div className="mx-2 py-1 border border-gray-300 rounded p-1">
+          {priority === 4 && <img src={urgentPriority} alt="Urgent" />}
+          {priority === 3 && <img src={highPriority} alt="High" />}
+          {priority === 2 && <img src={mediumPriority} alt="Medium" />}
+          {priority === 1 && <img src={lowPriority} alt="Urgent" />}
+          {priority === 0 && <img src={noPriority} alt="No priority" />}
         </div>
         <div className="flex items-center border border-gray-300 rounded p-1 ml-2">
-          <div className="w-2 h-2 bg-blue-500 rounded-full mr-2"></div>
+          <div className="w-2 h-2 bg-gray-400 rounded-full mr-2"></div>
           <div className="text-gray-500">{tag}</div>
         </div>
       </div>
